9ba21ddd8179c4777d35be5d11a60c36523ea725
gtconcha13-gif <gtconcha13@gmail.com>
2026-03-30 00:30:30 +0800

    commit -m "update the begin.py file with new printer information and formatting"

--- begin.py
+++ begin.py
@@ -1,1 +1,9 @@
 print("Welcome to the Matrix")
+company_name = "Chella Print Services"
+printing_price_bw = 3.0
+printing_price_color = 5.0
+printer_model = "Brother HL-L2350DW"
+printer_status = "Online"
+print(f"Company Name: {company_name}")
+format(printing_price_color, ".2f")
+print(printing_price_color)
